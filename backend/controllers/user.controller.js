583dc4fe7ab3427db950b89f4374be4923a9dbd5
kanhaiya1202 <kanhaiyasharma1202@gamil.com>
2025-07-14 15:33:32 +0530

    CreateLoginRoute

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import userModel from "../model/user.model.js";
 import * as userService from "../services/user.service.js";
-import { validationResult } from 'express-validator'  
+import { body, validationResult } from 'express-validator'  
 
 export const CreateUserController = async (req,res)=> {
     const errors = validationResult(req);
@@ -18,5 +18,34 @@ export const CreateUserController = async (req,res)=> {
         }
 }
 
+export const loginController = async (req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    try {
+        const {email, password} = req.body;
+        const user = await userModel.findOne({email}).select("+password")
+
+        if(!user){
+            return res.status(401).json({
+                errors:"Invalid credential"
+            })
+        }
+        const isMatch = await user.isValidPassword(password)
+        if(!isMatch){
+            return res.status(401).json({
+                errors:"Invalid credential"
+            })
+        }
+
+        const token = await user.generateJWT
+        res.status(200).json({user,token})
+    } catch(err){
+        res.status(400).send(err.message)
+
+    }
+}
 
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as userController from "../controllers/user.controller.js";
 import { body } from "express-validator";
+import bodyParser from "body-parser";
 
 const router = Router();
 
@@ -10,4 +11,9 @@ router.post("/register",
 
     userController.CreateUserController)
 
+router.post("/login",
+    body('email').isEmail().withMessage("Email must be a valid email"),
+    body('password').isLength({ min: 6 }).withMessage("Password must be at least 6 character long"),
+    
+    userController.loginController)
 export default router
