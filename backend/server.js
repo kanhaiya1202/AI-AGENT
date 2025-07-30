6705d97a36433c165aca92a439c2f8da69312fed
kanhaiya1202 <kanhaiyasharma1202@gamil.com>
2025-07-30 22:36:20 +0530

    socket middleware done

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,39 @@ import dotenv from 'dotenv/config.js';
 import app from './app.js';
 import http from 'http'
 import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken'
 
 
 const server = http.createServer(app);
-
 const io = new Server(server);
+
 io.on('connection', socket => {
     console.log('user connected')
   socket.on('event', data => { /* … */ });
   socket.on('disconnect', () => { /* … */ });
 });
+
+
+io.use((socket,next)=>{
+  try {
+    const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[1];
+
+    if(!token){
+      return next(new Error("authorization error"));
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if(!decoded){
+      return next(new Error("authorization error"))
+    }
+
+    socket.user = decoded
+
+    next();
+  } catch (error) {
+    next(error)
+  }
+})
+
 
 const port = process.env.PORT || 3000
 
