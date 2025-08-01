7a27e711f0dad4823f8c0775d2571a03a1cb5f18
kanhaiya1202 <kanhaiyasharma1202@gamil.com>
2025-08-01 10:38:12 +0530

    ai gemini key setUp

--- backend/routes/ai.routes.js
+++ backend/routes/ai.routes.js
@@ -5,4 +5,4 @@ const router = Router();
 router.get('/get-result', aiController.getResult)
 
 
-export default router
+export default router;
--- backend/services/ai.services.js
+++ backend/services/ai.services.js
@@ -1,9 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-
 // Access your API key as an environment variable
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export const generateResult = async (prompt) => {
   const result = await model.generateContent(prompt)
